9d81a1a310e4b464b6c529b22cb2db1720991b8f
Simon Gradin <simgr011@student.liu.se>
2023-05-03 03:25:17 +0200

    started work on running backend commands through new webpage

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const childprocess = require('child_process');
 const express = require('express');
 const path = require('path');
 
@@ -11,6 +12,26 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'htmlpage.html'));
 });
 
+app.get('/generation', (req, res) => {
+  res.sendFile(path.join(__dirname, 'data_generation.html'));
+});
+
+app.get('/full_backend_generation', (req, res) => {
+  //works
+    console.log("BACKEND COMMANDO " + req.query.path + "   " + req.query.factor)
+    let post_repo = () => {
+
+      console.log("done with generation")
+    }
+
+    let child = childprocess.exec("/target/release/gitdiffjson.exe repo " + req.query.path)
+    //let child = childprocess.exec("type nul > your_file.txt")
+    child.stdout.pipe(process.stdout)
+    child.on('exit', post_repo)
+
+});
+
+const sleep = ms => new Promise(r => setTimeout(r, ms));
 // Serve other files in the path
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, req.url));
@@ -19,5 +40,5 @@ app.get('*', (req, res) => {
 // Start the server
 const port = 5500;
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on  http://localhost:${port}`);
 });
